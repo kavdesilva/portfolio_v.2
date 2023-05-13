f3ae5743c5bd75e8d6fae2c1a354d7f8232f206a
Kristina <kavdesilva@gmail.com>
2023-05-12 23:05:38 -0400

    added clear button, new section for projects array.

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,30 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Projects = () => {
     const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Vue', 'GIT', 'GitHub', 'SQL', 'Express', 'MongoDB', 'Python', 'Django']
 
-    const [skillSelected, setButtonClass] = useState([]);
+    const [skillSelected, setButtonClass] = useState([])
 
-    const handleButtonClass = (index) => {
-        if (skillSelected.includes(index)){
-            setButtonClass(skillSelected.filter((i)=> i !== index))
+    const handleButtonClass = (selected) => {
+        if (skillSelected.includes(selected)){
+            setButtonClass(skillSelected.filter((skill)=> skill !== selected))
         } else {
-            setButtonClass([...skillSelected, index])
+            setButtonClass([...skillSelected, selected])
         }
-      
     };
+
+    useEffect(() => {
+        console.log(skillSelected)
+    }, [skillSelected])
 
     return (
         <div id="projects">
             <h2>Projects</h2>
             <div className="divider"></div>
             <div className="skills">
-                    <h3>My Skills</h3>
-                    {
-                        skills.map((skill, i) => (
-                            <button key={i} className={skillSelected.includes(i) ? "btn-skill btn-selected" : "btn-skill"} onClick={() => handleButtonClass(i)}><p className="gradient-text">{skill}</p></button>
-                        ))
-                    }
+                <h3>My Skills</h3>
+                {
+                    skills.map((skill, i) => (
+                        <button key={i} className={skillSelected.includes(skill) ? "btn-skill btn-selected" : "btn-skill"} onClick={() => handleButtonClass(skill)}><p className="gradient-text">{skill}</p></button>
+                    ))
+                }
+                <br />
+                <button className="btn-skill" onClick={() => setButtonClass([])}><p className="gradient-text">Clear</p></button>
+            </div>
+            <div className="projects-container">
+                <p>Projects array goes here.</p>
             </div>
         </div>
     )
