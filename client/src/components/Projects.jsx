6f9e6376adfbe2a9cb21e182e638adbe6c17a0a4
Kristina <kavdesilva@gmail.com>
2023-05-13 13:35:05 -0400

    created projects api endpoint, & functionality to filter projects by skills selected in project component.

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -11,11 +11,31 @@ const Projects = ({targetRefs}) => {
         } else {
             setButtonClass([...skillSelected, selected])
         }
-    };
+    }
+
+    const [projectsArray, setProjectsArray] = useState(null)
+    const [selectedProjects, setSelectedProjects] = useState([])
+
+    const dummyImgUrl = 'https://d33wubrfki0l68.cloudfront.net/19c708670a1f21231c1e2efa6ba38dbf52b15343/3237e/assets/jpeg/dopefolio.jpeg'
 
     useEffect(() => {
-        console.log(skillSelected)
-    }, [skillSelected])
+        fetch('/api/projects')
+            .then(res => res.json())
+            .then(data => setProjectsArray(data))
+            .catch(error => console.error(error))
+    }, [])
+
+    useEffect(() => {
+        if (!projectsArray) return;
+        let filteredProjects = [...projectsArray];
+        if (skillSelected.length > 0) {
+            filteredProjects = projectsArray.filter(project => {
+                return skillSelected.some(skill => project.tags.includes(skill));
+            });
+        }
+        setSelectedProjects(filteredProjects);
+    }, [skillSelected, projectsArray]);
+
 
     return (
         <div id="projects">
@@ -32,7 +52,22 @@ const Projects = ({targetRefs}) => {
                 <button className="btn-skill" onClick={() => setButtonClass([])}><p className="gradient-text">Clear</p></button>
             </div>
             <div className="projects-container">
-                <p>Projects array goes here.</p>
+                {
+                    selectedProjects?.map((project, i) => (
+                        <div key={i} className="project-card">
+                            <div className="project-image">
+                                <img src={dummyImgUrl} alt={project.img_alt} />
+                            </div>
+                            <div className="project-info">
+                                <p>{project.title}</p>
+                                <p>{project.description}</p>
+                                <ul>
+                                    {project.tags.map((tag) => {return <li>{tag}</li>})}
+                                </ul>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
---
+++ client/src/pages/api/projects.js
@@ -0,0 +1,43 @@
+export default (req, res) => {
+    const projectsArray = [
+        {
+            title: 'Moonsweepr',
+            url: 'http://moonsweepr.surge.sh/',
+            github_url: 'https://github.com/kavdesilva/moonsweepr',
+            img_src: '#',
+            img_alt: '',
+            description: 'A space-themed minesweeper game.',
+            tags: ['HTML', 'CSS', 'JavaScript']
+        },
+        {
+            title: `Children's Author Website Template`,
+            url: '#',
+            github_url: 'https://github.com/kavdesilva/author_website',
+            img_src: '#',
+            img_alt: '',
+            description: `An interactive website for an author's collection of works.`,
+            tags: ['React', 'MongoDB', 'Express', 'Node']
+        },
+        {
+            title: 'CycleList',
+            url: 'https://mighty-scrubland-76268.herokuapp.com/',
+            github_url: 'https://github.com/frank-booth/CycleList_Frontend',
+            github_url_2: 'https://github.com/frank-booth/CycleList_Backend',
+            img_src: '#',
+            img_alt: '',
+            description: 'A collaborative effort by a team of three developers, CycleList is a web application for creating cycle routines to your favorite tunes.',
+            tags: ['React', 'SQL', 'PostgreSQL', 'Express', 'Node']
+        },
+        {
+            title: 'Hekate',
+            url: '#',
+            github_url: 'https://github.com/kavdesilva/Hekate',
+            img_src: '#',
+            img_alt: '',
+            description: 'A period tracking app focused on gender-neutrality and data security.',
+            tags: ['Vue', 'Pinia', 'SQL', 'PostgreSQL', 'Express', 'Node']
+        }
+    ];
+    res.status(200).json(projectsArray);
+  };
+  
