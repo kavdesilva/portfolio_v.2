cce40b1d0531594dc0bf195222cd2479e2280755
Kristina <kavdesilva@gmail.com>
2023-05-12 21:48:54 -0400

    rearranged skills & added functionality.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,31 +2,16 @@ import { useRouter } from "next/router"
 
 const About = () => {
     const router = useRouter()
-
-    const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Vue', 'GIT', 'GitHub', 'SQL', 'Express', 'MongoDB', 'Python', 'Django']
-
     return (
         <div id="about">
             <h2>About Me</h2>
             <div className="divider"></div>
-            <div className="blurb">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dignissimos eveniet, dolores, maxime culpa ea veritatis, nulla omnis voluptates cupiditate voluptatem! Ducimus, modi! Vitae commodi iure aperiam minus tenetur, consequuntur magnam dolor architecto corrupti facere cumque perspiciatis eius nemo sint? Eveniet beatae eligendi quis culpa praesentium fuga nam aut inventore.</p>
-            </div>
             <div className="story-skills-container">
                 <div className="story">
                     <h3>My Story</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo perferendis quae dolore tempora modi quidem qui voluptas sunt error minus amet illum adipisci sit quo eligendi, quisquam repudiandae, pariatur facilis!</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos consequuntur consequatur commodi optio! Vel itaque adipisci optio tenetur totam voluptate perspiciatis vero quisquam soluta molestias laborum temporibus voluptas, eaque quidem omnis aut suscipit nulla iste incidunt. Repellat, aliquid? Voluptas, doloremque neque! Harum non quae sit nam asperiores iusto repudiandae inventore ipsum rerum sequi. Perferendis quibusdam, harum suscipit reiciendis porro rerum.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, atque similique, enim quae dolor non incidunt modi iste nihil placeat labore molestiae minima commodi perferendis.</p>
                     <button className="btn-secondary" onClick={() => router.push('/#contact')}>Contact</button>
-                </div>
-                <div className="skills">
-                    <h3>My Skills</h3>
-                    {
-                        skills.map((skill, i) => (
-                            <button className="btn-skill" key={i}><p className="gradient-text">{skill}</p></button>
-                        ))
-                    }
                 </div>
             </div>
         </div>
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 const Projects = () => {
+    const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Vue', 'GIT', 'GitHub', 'SQL', 'Express', 'MongoDB', 'Python', 'Django']
+
+    const [skillSelected, setButtonClass] = useState([]);
+
+    const handleButtonClass = (index) => {
+        if (skillSelected.includes(index)){
+            setButtonClass(skillSelected.filter((i)=> i !== index))
+        } else {
+            setButtonClass([...skillSelected, index])
+        }
+      
+    };
+
     return (
         <div id="projects">
             <h2>Projects</h2>
             <div className="divider"></div>
-            <div className="blurb">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dignissimos eveniet, dolores, maxime culpa ea veritatis, nulla omnis voluptates cupiditate voluptatem! Ducimus, modi! Vitae commodi iure aperiam minus tenetur, consequuntur magnam dolor architecto corrupti facere cumque perspiciatis eius nemo sint? Eveniet beatae eligendi quis culpa praesentium fuga nam aut inventore.</p>
+            <div className="skills">
+                    <h3>My Skills</h3>
+                    {
+                        skills.map((skill, i) => (
+                            <button key={i} className={skillSelected.includes(i) ? "btn-skill btn-selected" : "btn-skill"} onClick={() => handleButtonClass(i)}><p className="gradient-text">{skill}</p></button>
+                        ))
+                    }
             </div>
         </div>
     )
